78ff02b2056f1e27a7d54ea669e2fa6eaaebdf37
Danilo Ferreira <shymmi@hotmail.com>
2021-11-26 00:21:32 -0300

    models folder has been added

---
+++ MarvelSPA/Models/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelSPA.Models
+{
+    public class Data
+    {
+        public List<Personagem> results { get; set; }
+    }
+}
---
+++ MarvelSPA/Models/Resposta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelSPA.Models
+{
+    public class Resposta
+    {
+        public Data data { get; set; }
+    }
+}
